333106e8ad59dfa42e19734e4e8a15db4d28d328
quentinb <q.berlemont@geolid.com>
2018-09-17 12:18:58 +0200

    shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NgbModule
+  ],
+  exports: [
+    CommonModule,
+    NgbModule
+  ]
+})
+export class SharedModule { }
